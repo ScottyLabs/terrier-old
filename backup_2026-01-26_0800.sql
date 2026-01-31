5677fa2c9caf1fa3e5a774c9f56418f713d73875
Theodor Urban <theo.s.urban@gmail.com>
2026-01-31 15:53:53 -0500

    remove failed sql backups

--- backup_2026-01-26_0800.sql
+++
@@ -1,1 +0,0 @@
-cat: /home/deploy/terrier/backups/backup_2026-01-26_0800.sql: No such file or directory
--- backup_2026-01-26_0900.sql
+++
@@ -1,1 +0,0 @@
-cat: /home/deploy/terrier/backups/backup_2026-01-26_0900.sql: No such file or directory
