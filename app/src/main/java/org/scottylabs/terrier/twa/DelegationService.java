3430dbe67f94dc9c1cfc253f7513bf2627368c24
Krit Dass <dasskrit@gmail.com>
2026-01-31 16:51:51 -0500

    Initialize bubblewrap

---
+++ app/src/main/java/org/scottylabs/terrier/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.scottylabs.terrier.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
